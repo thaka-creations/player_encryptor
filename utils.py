594d86983551f5b24edb06667c608eac3dcf875e
abdulnelfrank <nelfrankaj@gmail.com>
2023-01-02 10:00:28 +0300

    feat: add file properties

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import json
+import os
 import requests
+import ffmpeg
 
 
 def list_products():
@@ -17,16 +20,39 @@ def get_file_contents():
         return [line.strip() for line in lines]
 
 
+def write_file_properties(contents):
+    # get file properties
+    file_list = [
+        {
+            "name": os.path.basename(element),
+            "size": os.stat(element).st_size,
+            "extension": os.path.splitext(element)[1],
+            "duration": ffmpeg.input(element).metadata['duration'],
+            "frame_rate": ffmpeg.input(element).metadata['fps']
+        }
+        for element in contents
+    ]
+
+    with open("properties.json", "w") as f:
+        json.dump(file_list, f)
+
+
 def write_lines_to_file(lines):
     with open("file.txt", "w"
               ) as f:
         for element in lines:
             f.write(str(element) + '\n')
 
+    # get file properties
+    write_file_properties(lines)
+
 
 def write_file(contents):
     with open("file.txt", "w") as f:
         f.write(contents)
+
+    # get file properties
+    write_file_properties(contents)
 
 
 def retrieve_product(request_id):
